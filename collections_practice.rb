2becc631a70c39bc6ee70a93488809d57f3f2757
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-10 16:28:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,5 +37,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-array.each_with_index.collect{|element, index| index[0, 2...] element +"s"}
+array.each_with_index.collect{|element, index| index[0, 2...] element += "s"}
 end
